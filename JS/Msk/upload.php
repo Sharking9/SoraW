4e413254968a9f78a123f56de4ffccd479a469b0
Sharking9 <manafhadi101@gmail.com>
2024-04-18 00:10:57 +0900

    Web Optimize

--- JS/Msk/upload.php
+++ JS/Msk/upload.php
@@ -1,9 +1,20 @@
 <?php
 $uploadDir = 'uploads/';
-$uploadedFile = $uploadDir . basename($_FILES['file']['name']);
+$fileName = 'tegami01'; // Nama file yang diinginkan
+$fileExtension = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION); // Ekstensi file yang diunggah
 
-if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadedFile)) {
-    echo json_encode(array('success' => true));
+// Hapus file yang sudah ada jika ada
+$existingFileName = $uploadDir . $fileName . '.' . $fileExtension;
+if (file_exists($existingFileName)) {
+    unlink($existingFileName);
+}
+
+// Gabungkan nama file baru dengan ekstensi
+$newFileName = $uploadDir . $fileName . '.' . $fileExtension;
+
+// Pindahkan file yang diunggah ke lokasi baru dengan nama baru
+if (move_uploaded_file($_FILES['file']['tmp_name'], $newFileName)) {
+    echo json_encode(array('success' => true, 'newFileName' => $newFileName));
 } else {
     echo json_encode(array('success' => false));
 }
